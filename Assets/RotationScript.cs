634dd9da655807c6ee73486089c82e2c75b11552
gamesketches <sam.von.ehren@gmail.com>
2016-02-05 17:20:15 -0500

    Got a rotate arund script woop woop

---
+++ Assets/RotationScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+
+	public GameObject directionalLight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(directionalLight.transform.position, Vector3.up, 1);
+	}
+}
